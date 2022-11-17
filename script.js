a239814dd00683805560b70ee2103156e9ddcf50
uvarov <uvarov.1337@gmail.com>
2022-11-17 23:27:46 +0200

    made a askGrid() func for Custom grid size button. Also made a activateEraser() func for Toggle eraser button, modified draw() and changeColor() funcs

--- script.js
+++ script.js
@@ -4,65 +4,122 @@ const grid4 = document.querySelector('.grid4')
 const grid16 = document.querySelector('.grid16');
 const grid32 = document.querySelector('.grid32');
 const grid64 = document.querySelector('.grid64');
-const clear = document.querySelector('.clear')
-
+const custom = document.querySelector('.custom');
+const clear = document.querySelector('.clear');
+const eraser = document.querySelector('#eraser');
 
 let div;
 let rowDiv;
+
+container.addEventListener('click', draw);
 
 grid4.addEventListener('click', createGrid4);
 grid16.addEventListener('click', createGrid16);
 grid32.addEventListener('click', createGrid32);
 grid64.addEventListener('click', createGrid64);
-container.addEventListener('click', draw);
+custom.addEventListener('click', askGrid);
 clear.addEventListener('click', clearGrid);
+eraser.addEventListener('click', activateEraser);
 
-function draw(e) 
+function activateEraser (e)
 {
-    
-    let targetId = e.target.parentNode.parentNode.id;
-    
-    console.log(`Target id: ${targetId}`);
 
-    if (targetId == "container" && e.target.parentNode.parentNode.className == 'ready') 
+    let target = e.target;
+    
+    
+    if (target.id == 'eraser' && target.dataset.value == 'active')
     {
-        container.classList.remove('ready');
-        console.log(`container.className: ${container.className}`);
+       delete target.dataset.value;
+       eraser.classList.remove('active');
+        console.log(eraser.dataset.value);
+        // console.log(container.classList);
+    }
+    else
+    {   
+        target.dataset.value = 'active';
+        eraser.className = '';
+        eraser.className += 'active';
         
-    } 
-    else 
-    { 
-        container.className = '';
-        container.className += 'ready';
-        console.log(`container.className: ${container.className}`);
-        
+        // console.log( container.classList);
     }
 
     changeColor();
 
 }
 
+function askGrid () 
+{
+    let size = +prompt('Inser grid size. Max value is under 100', '100');
+    
+    removeGrid();
+    for (let i = 0; i < `${size}`; i++)
+    {
+        createRow();
 
-////////////////////////////////////////////////////////////////////////////////////////////////
+        for (let j = 0; j < `${size}`; j++)
+        {   
+            createDiv();    
+        }
+    }
+}
 
+function draw(e) 
+{
+    
+    let targetId = e.target.parentNode.parentNode.id;
+    let targetTile = e.target;
+
+    // console.log(targetTile);
+    
+    console.log(`Target id: ${targetId}`);
+
+    if (targetId == "container" && e.target.parentNode.parentNode.className == 'ready') 
+    {
+
+        container.classList.remove('ready');
+        console.log(`container.className: ${container.className}`);
+        
+    } 
+    else 
+    { 
+        
+        if (!(eraser.dataset.value == 'active'))
+        {
+            container.className = '';
+            container.className += 'ready';
+        }
+        
+        console.log(`container.className: ${container.className}`);
+
+        if (targetTile.className == 'tile' && !(eraser.dataset.value == 'active'))
+        {
+            targetTile.classList += ' tileHovered';
+        }
+
+    }
+
+    changeColor();
+
+}
 
 function changeColor()
 {
-    let  tiles = document.querySelectorAll('.tile');
+    let tiles = document.querySelectorAll('.tile');
     
     for (const tile of tiles) {
-
+        
             tile.addEventListener('mouseover', function hoverTile() {
                 if (container.className == 'ready')
                 {
                     tile.classList.add("tileHovered");
                 }   
+                if (eraser.dataset.value == 'active')
+                {
+                    tile.classList.remove("tileHovered");
+                }
               }); 
       }   
 }
-
-
-////////////////////////////////////////////////////////////////////////////////////////////////
 
 function createRow()
 {
